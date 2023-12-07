bf6b4a4ecc889f531eeb49bd6299fce51ff83fd7
AHMEDmohamedyassin <ahmedmohamed982025@outlook.com>
2023-12-07 03:54:00 +0200

    v4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='w-full bg-slate-200 text-gray-900 text-center py-4'>
-        made by <a className='font-extrabold text-sky-500' href='https://www.cvhub.site/ahmed/about'>Ahmed yassin</a>
+        made by <a className='font-extrabold text-sky-500' href='https://www.cvhub.site/ahmed/about'>Ahmed yassin</a> <span>section : 2 , BN : 4 , ID : 9210150</span>
     </div>
   )
 }
